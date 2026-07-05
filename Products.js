9b10ec774fdd07af826021d57efaf054326c3fb6
rajababunuq-ai <rajababunuq@gmail.com>
2026-07-05 13:46:15 +0530

    Create Products.js

---
+++ Products.js
@@ -0,0 +1,32 @@
+// =========================
+// Products Data
+// =========================
+
+const products = [
+{
+id:1,
+name:"Running Shoes",
+category:"Shoes",
+price:2999,
+image:"https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500"
+},
+{
+id:2,
+name:"Sports Watch",
+category:"Watch",
+price:1999,
+image:"https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500"
+},
+{
+id:3,
+name:"Wireless Headphones",
+category:"Headphone",
+price:2499,
+image:"https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500"
+},
+{
+id:4,
+name:"Casual Shoes",
+category:"Shoes",
+price:3499,
+image:"https
